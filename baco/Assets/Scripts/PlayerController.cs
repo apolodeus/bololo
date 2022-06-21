03a3fa8970c070433ee62d9b89439f4245f9d166
Willian Vinícius Damasceno Costa <damasceno.willian@academico.ifrn.edu.br>
2022-06-21 08:31:18 -0300

    baco

--- baco/Assets/Scripts/PlayerController.cs
+++ baco/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     private Camera _mainCamera;
     private Vector2 _moveInput;
     private Rigidbody _rigidbody;
-    private object moveMultiplier;
+    public float moveMultiplier;
 
     private void OnEnable()
     {
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnActionTriggered(InputAction.CallbackContext obj)
     {
-        if (obj.action.name.CompareTo(_gameControls.gameplay.move.name) != 0)
+        if (obj.action.name.CompareTo(_gameControls.gameplay.move.name) == 0)
         {
             _moveInput = obj.ReadValue<Vector2>();
         }
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        _rigidbody.AddForce((_mainCamera.transform.forward * _moveInput.y +  _mainCamera.transform.right * _moveInput.x ) * (float) moveMultiplier * Time.deltaTime);
+        _rigidbody.AddForce((_mainCamera.transform.forward * _moveInput.y +  _mainCamera.transform.right * _moveInput.x ) *  moveMultiplier * Time.fixedDeltaTime);
     }
 
     private void FixedUpdate()
